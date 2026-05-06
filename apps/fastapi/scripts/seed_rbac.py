d4a88eab4ad2b13f3481b5f6ef52c6fa6bb4bf22
Codewithash27 <hukkerikaraman04@gmail.com>
2026-05-06 12:55:03 +0530

    seed file

--- apps/fastapi/scripts/seed_rbac.py
+++ apps/fastapi/scripts/seed_rbac.py
@@ -83,6 +83,7 @@ def seed_rbac_data():
                     {"name": "Fee Discount", "path": "/fees/discounts", "feature": "FEE_MGMT"},
                     {"name": "Fee Installment Status", "path": "/fees/installment-status", "feature": "FEE_MGMT"},
                     {"name": "Assign Student Fee", "path": "/fees/assign-student-fee", "feature": "FEE_MGMT"},
+                    {"name": "Fee Report", "path": "/fees/reports", "feature": "FEE_MGMT"},
                 ]
             },
             {
